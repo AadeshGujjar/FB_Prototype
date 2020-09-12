202c587f2836f20c3a75d0f605972002617cd046
Aadesh Gujjar <aadesh.gujjar25@gmail.com>
2020-09-12 23:04:08 +0530

    Online Users.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_facebook_responsive_ui/data/data.dart';
 import 'package:flutter_facebook_responsive_ui/widgets/circle_button.dart';
 import 'package:flutter_facebook_responsive_ui/widgets/create_post_container.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/rooms.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -28,9 +29,16 @@ class HomeScreen extends StatelessWidget {
               CircleButton(icon: MdiIcons.facebookMessenger, iconSize: 30.0, onPressed: ()=>('Messenger'))
             ],
           ),
-          SliverToBoxAdapter(
-            child: CreatePostContainer(currentUser: currentUser),
+          SliverPadding(
+            padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 5.0),
+            sliver: SliverToBoxAdapter(
+              child: CreatePostContainer(currentUser: currentUser),
+            ),
           ),
+
+          SliverToBoxAdapter(
+            child: Rooms(onlineUsers: onlineUsers),
+          )
 
 
         ],
